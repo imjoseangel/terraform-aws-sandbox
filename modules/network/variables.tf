6d1b21a433e3e7b84c27c29522a9faa86f3e61ec
imjoseangel <josea.munoz@gmail.com>
2022-07-20 23:10:02 +0200

    add vars

---
+++ modules/network/variables.tf
@@ -0,0 +1,14 @@
+#-------------------------------
+# Common Variables
+#-------------------------------
+variable "env" {
+  type        = string
+  description = "Name of the environment."
+  default     = "sandbox"
+}
+
+variable "region" {
+  type        = string
+  description = "aws region"
+  default     = "eu-central-1"
+}
